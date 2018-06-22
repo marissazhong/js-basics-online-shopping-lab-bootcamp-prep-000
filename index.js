513d93d1b5f97310d54162e2ec0c54e0df8c363d
Marissa Zhong <mwz2101@columbia.edu>
2018-06-22 17:12:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,13 +18,13 @@ function viewCart() {
   if (cart.length === 0) {
     return 'Your shopping cart is empty.';
   } else if (cart.length === 1) {
-    return `In your cart, you have ${cart[0].itemName} at $${cart[0]itemPrice}`;
+    return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}`;
   }
     result = 'In your cart, you have ';
     for (let i = 0; i < cart.length-1; i++) {
-      result += cart[i].itemName + ' at $' + cart[i].itemPrice + ', '
+      result += `${cart[i].itemName} at $${cart[i].itemPrice}, `;
     }
-    result += cart[-1].itemName + ' at $' + cart[-1].itemPrice + '.';
+    result += `and ${cart[-1].itemName} at $${cart[-1].itemName}.`;
   }
   return result;
 }
