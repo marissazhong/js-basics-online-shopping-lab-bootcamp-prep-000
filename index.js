8740c7bae630a798ca5816806e82031e14303c0b
Marissa Zhong <mwz2101@columbia.edu>
2018-06-22 17:20:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -40,9 +40,11 @@ function total() {
 function removeFromCart(item) {
   for (let i = 0; i < cart.length; i++) {
     if (cart[i].itemName === item) {
-      cart = cart.splice();
+      cart = cart.splice(i,1);
+      return cart;
     }
   }
+  return 'That item is not in your cart.';
 }
 
 function placeOrder(cardNumber) {
