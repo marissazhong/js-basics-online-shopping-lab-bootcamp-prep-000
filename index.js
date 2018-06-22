079a98546faccd054a471c7a540d1d58c23bdff3
Marissa Zhong <mwz2101@columbia.edu>
2018-06-22 17:31:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,6 @@ function addToCart(item) {
 }
 
 function viewCart() {
-  console.log(cart);
   if (cart.length === 0) {
     return 'Your shopping cart is empty.';
   } else if (cart.length === 1) {
